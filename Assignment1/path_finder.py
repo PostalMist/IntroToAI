fedc17c4f2987eca25a82181d9d1de95a74d9313
Reme <31744912+PostalMist@users.noreply.github.com>
2018-07-29 00:19:55 -0400

    Update path_finder.py

--- Assignment1/path_finder.py
+++ Assignment1/path_finder.py
@@ -9,10 +9,19 @@ class Node:
         self.g = g # path cost
         self.h = h # heuristic
         self.parent = parent
-    def update_f():
+    def update_f(self):
         self.f = self.g + self.h
-    def location():
+    def location(self):
         return self.x, self.y
+########################################################################################
+### global variables
+########################################################################################
+START_NODE
+GOAL_NODE
+WEIGHT = 1
+ZERO_HEURISTICS = 1
+numExpansions = 0
+numSteps = 0
 
 def generateNeighbors(map, origin_node):
     """
@@ -25,13 +34,36 @@ def generateNeighbors(map, origin_node):
 
     neighbors = []
     x, y = origin_node.location()
+    if(x - 1 < 0):
+        pass
+    else:
+        if(map[x-1][y] == 1):#getting UP
+            neighbors.append(Node(x-1,y,1))
+    if(x + 1 >= len(map)):
+        pass
+    else:
+        if(map[x+1][y] == 1):# getting DOWN
+            neighbors.append(Node(x+1,y,1))
+           
+    if(y - 1 < 0):
+        pass
+    else:
+        if(map[x][y-1] == 1):#getting LEFT
+            neighbors.append(Node(x,y-1,1))
+    if(y + 1 >= len(map[x])):
+        pass
+    else:
+        if(map[x][y+1] == 1):# getting RIGHT
+            neighbors.append(Node(x,y+1,1))
+    return neighbors
+def h(origin_node, goal_node): 
+    """ function will compute the heuristic. It will compute the manhattan distance if zero_heuristic variable is one and
+    and the zero heuristic if zero is zero """
+    return ZERO_HEURISTIC * round( math.sqrt(((goal_node.x - origin_node.x)**2)+((goal_node.y - origin_node.y)**2)), 2)
 
-    if(x == 0): # not done
-        left = Node()
-        if()
-        neighbors.append(left)
-        neighbors.append(right)
-
+def g(origin_node, node):
+    """ returns cost from the origin node to any node """
+    return round( math.sqrt(((node.x - origin_node.x)**2)+((node.y - origin_node.y)**2)), 2)
 def PathFinder(map, initial_x, initial_y, goal_x, goal_y):
     """
         Args:
